a8402ceda1163350f29787afc058e23d09c1cd8a
Romanov Oleg <oleg@ubuntu-512mb-nyc3-01lalkaYana.localdomain>
2017-02-10 20:36:25 +0000

    add a personal uri for each user

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.*; 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
 
 import hello.User;
 import hello.UserRepository;
@@ -14,19 +16,40 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/signup")
-    public String signup(@ModelAttribute User user, BindingResult bindingResult, Model model) {
+    @PostMapping("/{user_id}")
+    public ModelAndView signup(@ModelAttribute User user, @PathVariable Integer user_id, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             //errors processing
         }
-        userRepository.save( new User(user.getName(), user.getEmail()) );
+
+        // Add new user
+        userRepository.save(user);
         model.addAttribute("user", user);
-        return "result";
+
+        // Search last element
+        Iterable<User> iter = userRepository.findAll();
+
+		for(User cur_user : iter)
+			user = cur_user;
+
+		// update URI
+        return new ModelAndView(new RedirectView("/"+user.getId(), true));
     }
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("user", new User());
         return "index";
+    }
+
+    @GetMapping("/{user_id}")
+    public String myPage(@PathVariable Integer user_id, Model model) {
+        if (userRepository.exists(user_id)){
+    		User user = userRepository.findOne(user_id);
+    		model.addAttribute("user", user);
+    		return "result";
+        }
+
+        return "error";
     }
 }
