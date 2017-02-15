8743081924c9b1f46e284897fcc020a8d2a821ca
Romanov Oleg <oleg@ubuntu-512mb-nyc3-01lalkaYana.localdomain>
2017-02-15 23:29:39 +0000

    Add backend for login; add hash of password in DB

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import hello.User;
 import hello.UserRepository;
 
@@ -17,21 +19,32 @@ public class MainController {
     private UserRepository userRepository;
 
     @PostMapping("/{user_id}")
-    public ModelAndView signup(@ModelAttribute User new_user, @PathVariable Integer user_id, BindingResult bindingResult, Model model) {
+    public ModelAndView signup(@ModelAttribute User new_user, 
+                            @ModelAttribute String passwordConfirm,
+                            @PathVariable Integer user_id, 
+                            BindingResult bindingResult,
+                            Model model) {
+
         if (bindingResult.hasErrors()) {
             //errors processing
         }
 
         try{
-        	// Save new user
-	        User user = userRepository.save(new_user);
-	        if (user.getId() == 0) {
+            // Get hash from password
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedPassword = passwordEncoder.encode(new_user.getPassword());
+
+            // Save user with hash instead of a password
+            new_user.setPassword(hashedPassword);
+            User user = userRepository.save(new_user);
+    	        
+            if (user.getId() == 0) {
                 // user already exists
                 return new ModelAndView(new RedirectView("/", true));
             }
 
             model.addAttribute("user", user);
-            return new ModelAndView(new RedirectView("/" + user.getId(), true));
+            return new ModelAndView(new RedirectView("/" + user.getId(), true));               
         }catch(Exception e){
         	// do nothing
         }
@@ -41,16 +54,43 @@ public class MainController {
 
     @GetMapping("/{user_id}")
     public String myPage(@PathVariable Integer user_id, Model model) {
+        
         if (userRepository.exists(user_id)){
     		User user = userRepository.findOne(user_id);
     		model.addAttribute("user", user);
+
     		return "profile";
         }
 
         return "error";
     }
 
-    
+    @PostMapping("/log_in")
+    public ModelAndView login(Model model,
+                             @ModelAttribute User user) {
+        // Search user with right email
+        String rawPassword = user.getPassword();
+        user = findOneByEmail( user.getEmail() );
+
+        if (user.getId() == 0) 
+            return new ModelAndView(new RedirectView("/login", true));
+        
+        try{
+            // Check password
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedPassword = passwordEncoder.encode(rawPassword);
+        
+            if ( passwordEncoder.matches(rawPassword, user.getPassword()) ){
+                model.addAttribute("user", user);
+                return new ModelAndView(new RedirectView("/" + user.getId(), true));
+            }
+        }catch(Exception e){
+            // do nothing
+        }
+
+        return new ModelAndView(new RedirectView("/login", true));
+    }
+
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("user", new User());
@@ -60,6 +100,17 @@ public class MainController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("passwordConfirm", new String());
         return "index";
+    }
+
+    public User findOneByEmail(String email){
+        Iterable <User> iter = userRepository.findAll();
+
+        for(User cur_user : iter)
+            if ( email.equals(cur_user.getEmail()) )
+                return cur_user;
+        
+        return new User();
     }
 }
--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table( uniqueConstraints = { @UniqueConstraint( columnNames = { "name", "email" } ) } )
+@Table( uniqueConstraints = { @UniqueConstraint( columnNames = { "email" } ) } )
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -18,7 +18,13 @@ public class User {
 
     private String email;
 
+    private String password;
 
+    public User (String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
     public User (String name, String email) {
         this.name = name;
         this.email = email;
@@ -43,10 +49,19 @@ public class User {
         this.name = name;
     }
 
+
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
