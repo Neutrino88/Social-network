ea29ba9ec6c1fe991b17d940b147b99123a36ac4
Oleg Romanov <romanov-97@mail.ru>
2017-02-25 17:16:29 +0300

    Add validation in UsersController; Set up time in posts; Fix some bugs

--- src/main/java/vitmo/controllers/UsersController.java
+++ src/main/java/vitmo/controllers/UsersController.java
@@ -11,8 +11,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.text.DateFormat;
-
 @RestController
 @RequestMapping("/")
 public class UsersController {
@@ -34,16 +32,16 @@ public class UsersController {
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public ModelAndView addUser(String username, String password, String password_confirm) {
-        //no empty fields allowed
-        if (username.isEmpty() || password.isEmpty() || password_confirm.isEmpty())
-            return null;
-        //passwords should match
-        if (!password.equals(password_confirm))
-            return null;
+        // Check format fields
+        if (    !passwordInNormalFormat(password)  ||
+                !password.equals(password_confirm) ||
+                !usernameInNormalFormat(username) )
+            return new ModelAndView("index");
+        // Save new user
         try {
             User user = new User(username, password);
             users.save(user);
-            return new ModelAndView(new RedirectView("/user/" + user.getId(), true));
+            return new ModelAndView("login");
         } catch (Exception e) {
             return new ModelAndView("index");
         }
@@ -80,12 +78,11 @@ public class UsersController {
         User user = users.findByUsername(principal.getUsername());
 
         // if current password is wrong
-        if (!equalsPassword(cur_password, user)) {
+        if (!equalsPassword(cur_password, user))
             return new ModelAndView("/settings");
-        }
 
         // change username
-        if (username.length() >= 4) {
+        if (usernameInNormalFormat(username)) {
             try{
                 User user_from_db = users.findOne(user.getId());
                 user_from_db.setUsername(username);
@@ -95,7 +92,8 @@ public class UsersController {
             }
         }
         // change password
-        if ( new_password.length() >= 6  && new_password.equals(confirm_password) ) {
+        if (    passwordInNormalFormat(new_password)  &&
+                equalsPassword(new_password, confirm_password) ){
             try{
                 User user_from_db = users.findOne(user.getId());
                 user_from_db.setPassword(new_password);
@@ -109,6 +107,36 @@ public class UsersController {
     }
 
     private boolean equalsPassword(String password, User user){
+    // Compare password with user.password
         return password.equals(user.getPassword());
+    }
+    private boolean equalsPassword(String password, String confirm_password){
+        // Compare password with confirm_password
+        return password.equals(confirm_password);
+    }
+
+    private boolean usernameInNormalFormat(String username){
+        // Check length of username
+        if (username.length() < 4 || username.length() > 20)
+            return false;
+
+        // Check format every characters in username
+        for (char symbol : username.toCharArray()) {
+            int code = (int) symbol;
+
+            if (  !(48 <= code && code <= 57 ||     // if not in 0..9 and
+                    65 <= code && code <= 90 ||     // if not in A..Z and
+                    97 <= code && code <= 122 ||    // if not in a..z and
+                    (int) '_' == code))             // if is not '_'
+                return false;
+        }
+        return true;
+    }
+    private boolean passwordInNormalFormat(String password){
+        // Check length of username
+        if (password.length() < 6 || password.length() > 20)
+            return false;
+
+        return true;
     }
 }
